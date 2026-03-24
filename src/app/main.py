0f37fc163c4640d1c5d63ff28ef34946410a76de
Joao <joao.ribeiro@e-deploy.com.br>
2026-03-23 21:29:14 -0300

    register event handlers

--- src/app/main.py
+++ src/app/main.py
@@ -1,22 +1,42 @@
-#uvicorn main:app --reload
-import logging
+#pylint: disable=wildcard-import, redefined-outer-name, unused-import, unused-argument, unused-wildcard-import
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from fastapi.security import OAuth2PasswordBearer
 
-from .routes.auth_routes import auth_router
-from .routes.order_routes import order_router
-from .routes.payment_routes import payment_router
-from app.events_listeners.order_listeners import * # to register the event listeners
+from app.routers.auth_router import auth_router
+from app.routers.order_router import order_router
+from app.routers.payment_router import payment_router
+from app.db.events_listeners.order_listeners import * # to register the event listeners
 
-# Set up logging to see SQL statements
-# logging.basicConfig()
-# logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
-# logging.getLogger("sqlalchemy.orm").setLevel(logging.INFO)
-# logging.getLogger("sqlalchemy").setLevel(logging.ERROR)
+from app.events.dispatcher_instance import dispatcher
+from app.events.handlers.order_handlers import (
+    order_confirmed_handler,
+    order_ready_handler,
+    order_confirm_paid_handler,
+)
+from app.events.order_events import (
+    OrderConfirmedEvent,
+    OrderReadyEvent,
+    ConfirmPaidOrderEvent,
+)
 
-app = FastAPI()
 
-oauth2_schema = OAuth2PasswordBearer(tokenUrl="auth/signin")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+
+    dispatcher.register_handler(OrderConfirmedEvent, order_confirmed_handler)
+    dispatcher.register_handler(OrderReadyEvent, order_ready_handler)
+    dispatcher.register_handler(ConfirmPaidOrderEvent, order_confirm_paid_handler)
+
+    yield
+
+app = FastAPI(
+    title="Delivery API",
+    description="API for managing orders, payments and delivery of a food delivery service.",
+    version="1.0.0",
+    docs_url="/docs",
+    redoc_url="/redoc",
+    lifespan=lifespan,
+)
 
 app.include_router(order_router)
 app.include_router(auth_router)
